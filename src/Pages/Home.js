9accc01571385b291f4c620aa3bf94654756d44f
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-09-05 21:15:32 +0530

    axios dataload

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,16 @@
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  
+  const [users, setUsers] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    axios.get(url).then((data) => {
+      const loadData = data.data;
+      const userData = loadData.map((user) => user);
+      setUsers(userData);
+    });
+  }, []);
   return (
     <div>
       <h1>Skionetizy Team Members</h1>
